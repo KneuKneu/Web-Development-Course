fba964aad440095ee9ae9207ee75929d6cd71e9e
KneuKneu <45817272+KneuKneu@users.noreply.github.com>
2025-07-12 16:07:01 +0200

    Update index.js

--- Drum Kit/Drum Kit Starting Files/index.js
+++ Drum Kit/Drum Kit Starting Files/index.js
@@ -11,23 +11,41 @@ function playSound(event) {
       s.play();
       break;
     case "a":
-        var a = new Audio("sounds/snare.mp3");
-        a.play();
-        break;
+      var a = new Audio("sounds/snare.mp3");
+      a.play();
+      break;
+    case "d":
+      var d = new Audio("sounds/tom-1.mp3");
+      d.play();
+      break;
+    case "j":
+      var j = new Audio("sounds/tom-2.mp3");
+      j.play();
+      break;
+    case "k":
+      var k = new Audio("sounds/tom-3.mp3");
+      k.play();
+      break;
+    case "l":
+      var l = new Audio("sounds/tom-4.mp3");
+      l.play();
+      break;
     default:
       console.log("Wong Botton!!");
   }
 }
 
 function buttonAnimation(currentKey){
-  document.querySelector('.' + currentKey)
+  var activeButton = document.querySelector('.' + currentKey)
+  console.log('.pressed style applied?');
+  activeButton.classList.add('pressed');
 }
 // Adds an addEventListener to every button
 for (i = 0; i < list.length; i++) {
   list[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     playSound(buttonInnerHTML);
-    buttonAnimation(buttonInnerHTML)
+    buttonAnimation(buttonInnerHTML);
   });
 }
 

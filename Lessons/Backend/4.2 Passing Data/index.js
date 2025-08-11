ac6f285511ec8730b7a77074bbb75855f56df408
KneuKneu <45817272+KneuKneu@users.noreply.github.com>
2025-08-11 13:19:06 +0200

    Loads!
    Loads

--- Lessons/Backend/4.2 Passing Data/index.js
+++ Lessons/Backend/4.2 Passing Data/index.js
@@ -6,9 +6,30 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  console.log("Received a GET request");
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  console.log("Received a POST request");
+  console.log("Request body:", req.body);
+  var total = req.body.fName.length + req.body.lName.length
+  console.log("Response sent to client");
+  console.log("End of POST request handler");
+  console.log("====================================");
+  console.log("Returning to the main page");
+  res.render("index.ejs", { total: total });
+  // go back to the main page
+  res.redirect("/");
+  // res.send('test');
+  //change the h1 text to show the total number of characters in the names
+  // This line won't work in the server-side context, so we need to send the total
+  // back to the client in a different way.
+  // Instead, we can send the total as part of the response.
+  // res.send(`<h1 id='test'>Total characters in names: ${total}!</h1>`);
+  // Note: The above line will not work as expected because `document` is not defined
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
